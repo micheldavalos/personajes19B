8dceefffb288a58f0c4a3673f5d738b8845012bf
Michel Dávalos <michel.davalos@academicos.udg.mx>
2019-10-21 22:04:43 -0500

    creando repositorio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {FotoPage} from "../pages/foto/foto";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    FotoPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    FotoPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {FotoPage} from "../foto/foto";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  foto = FotoPage;
+  personajes = [
+    {
+      nombre: "Iron Man",
+      actor: "Robert Downey Jr.",
+      avatar: "../assets/imgs/iron.png",
+      foto: "../assets/imgs/robert.jpg"
+    },
+    {
+      nombre: "Thor",
+      actor: "Chris Hemsworth",
+      avatar: "../assets/imgs/thor.jpg",
+      foto: "https://upload.wikimedia.org/wikipedia/commons/e/e8/Chris_Hemsworth_by_Gage_Skidmore_2_%28cropped%29.jpg"
+    },
+    {
+      nombre: "Spider Man",
+      actor: "Tom Holland",
+      avatar: "../assets/imgs/spider.png",
+      foto: "../assets/imgs/robert.jpg"
+    },
+    {
+      nombre: "Captain America",
+      actor: "Chris Evans",
+      avatar: "../assets/imgs/captain.jpg",
+      foto: "../assets/imgs/robert.jpg"
+    }
+  ];
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  clickFoto(actor, foto) {
+    this.navCtrl.push(this.foto,
+      {actor: actor, foto: foto});
   }
 
 }
